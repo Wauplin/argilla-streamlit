d4a431d4793359a3dd68cd74cf1cd5a52dd645d4
davidberenstein1957 <david.m.berenstein@gmail.com>
2023-05-11 08:23:50 +0200

    chore: updated workspace and datasets getter

--- my_app/utils/commons.py
+++ my_app/utils/commons.py
@@ -110,30 +110,30 @@ def hf_login_flow():
 
 def get_dataset_list(api_url, api_key):
     client = ArgillaSingleton.init(api_url, api_key)._client
-    try:
-        url = "{}/api/datasets".format(client.base_url)
-
-        response = httpx.get(
+    url = "{}/api/workspaces".format(client.base_url)
+    response = httpx.get(
             url=url,
             headers=client.get_headers(),
             cookies=client.get_cookies(),
             timeout=client.get_timeout(),
         )
-        response.raise_for_status()
-        response.json()
-    except Exception:
-        url = "{}/api/datasets/".format(client.base_url)
+    response.raise_for_status()
+    worskpace_names = [w["name"] for w in response.json()]
 
+    datasets = []
+    for name in worskpace_names:
+        url = "{}/api/datasets?workspace={}".format(client.base_url, name)
         response = httpx.get(
-            url=url,
-            headers=client.get_headers(),
-            cookies=client.get_cookies(),
-            timeout=client.get_timeout(),
-        )
+                url=url,
+                headers=client.get_headers(),
+                cookies=client.get_cookies(),
+                timeout=client.get_timeout(),
+            )
         response.raise_for_status()
+        datasets += response.json()
 
     dataset_overview = []
-    for dataset in response.json():
+    for dataset in datasets:
         metadata = dataset["metadata"].values()
         if (
             metadata
